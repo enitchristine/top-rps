57521ef312cf6fc3963fcf4762700bf85a035e40
Christine <enit.christine@gmail.com>
2025-04-10 13:38:09 +1000

    Made playGame() where playRound is called 5 times and winner of the game is announced.

--- script.js
+++ script.js
@@ -53,10 +53,63 @@ function playRound(humanChoice,computerChoice){
     }
 }
 
-//global variables
-let humanScore=0;
-let computerScore=0;
+function playGame(){
 
-playRound(getHumanChoice(),getComputerChoice());
-console.log(humanScore);
-console.log(computerScore);
+    function playRound(humanChoice,computerChoice){
+        humanChoice=humanChoice.toLowerCase();
+    
+        console.log(humanChoice);
+        console.log(computerChoice);
+    
+        if (humanChoice===computerChoice){
+            console.log("Draw! You both chose "+humanChoice+".");
+            return;
+        }else if(humanChoice==="paper"){ // player chooses paper
+            if(computerChoice==="rock"){
+                console.log("You win! Paper beats rock!");
+                humanScore+=1;
+            } else if (computerChoice==="scissors"){
+                console.log("You lose :(. Scissors beat paper.")
+                computerScore+=1;
+            }
+        }else if(humanChoice==="rock"){ // player chooses rock
+            if(computerChoice==="scissors"){
+                console.log("You win! Rock beats scissors!");
+                humanScore+=1;
+            } else if (computerChoice==="paper"){
+                console.log("You lose :(. Paper beats rock.")
+                computerScore+=1;
+            }
+        }else if(humanChoice==="scissors"){ // player chooses scissors
+                if(computerChoice==="paper"){
+                    console.log("You win! Scissors beats paper!");
+                    humanScore+=1;
+                } else if
+                (computerChoice==="rock"){
+                    console.log("You lose :(. Rock beats scissors.")
+                    computerScore+=1;
+                }
+        }
+    }
+
+    let humanScore=0;
+    let computerScore=0;
+
+    for (let i=0; i<5; i++){
+        playRound(getHumanChoice(),getComputerChoice());
+        console.log(humanScore);
+        console.log(computerScore);
+    }
+
+    if (humanScore===computerScore){
+        console.log("Draw! Nobody wins :/");
+    }
+    else if (humanScore>computerScore){
+        console.log("You win! You beat the computer!");
+    }else{
+        console.log("You lose... to a computer?!")
+    }
+    
+}
+
+playGame();
