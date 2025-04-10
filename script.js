cb077dcb869367103086b0fe77c7ba6ff42e6d93
Christine <enit.christine@gmail.com>
2025-04-10 13:26:34 +1000

    Created function playRound() for game logic.

--- script.js
+++ script.js
@@ -17,8 +17,38 @@ function getHumanChoice(){
     return choice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice,computerChoice){
+    humanChoice=humanChoice.toLowerCase();
 
-// for (let i=0;i<3;i++){
-//     console.log(getComputerChoice());   
-// }
+    console.log(humanChoice);
+    console.log(computerChoice);
+
+    if (humanChoice===computerChoice){
+        console.log("Draw! You both chose "+humanChoice+".");
+        return;
+    }else if(humanChoice==="paper"){ // player chooses paper
+        if(computerChoice==="rock"){
+            console.log("You win! Paper beats rock!");
+        } else if (computerChoice==="scissors"){
+            console.log("You lose :(. Scissors beat paper.")
+        }
+    }else if(humanChoice==="rock"){ // player chooses rock
+        if(computerChoice==="scissors"){
+            console.log("You win! Rock beats scissors!");
+        } else if (computerChoice==="paper"){
+            console.log("You lose :(. Paper beats rock.")
+        }
+    }else if(humanChoice==="scissors"){ // player chooses scissors
+            if(computerChoice==="paper"){
+                console.log("You win! Scissors beats paper!");
+            } else if (computerChoice==="rock"){
+                console.log("You lose :(. Rock beats scissors.")
+            }
+    }
+}
+
+//global variables
+let humanScore=0;
+let computerScore=0;
+
+playRound(getHumanChoice(),getComputerChoice());
